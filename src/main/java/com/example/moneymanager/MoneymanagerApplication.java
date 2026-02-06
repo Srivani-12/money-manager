c8ca189f7c7a192814a90f96ebf4f233cf0aeea1
Srivani <emmadojusrivani@gmail.com>
2026-02-06 15:57:18 +0530

    remove mail

--- src/main/java/com/example/moneymanager/MoneymanagerApplication.java
+++ src/main/java/com/example/moneymanager/MoneymanagerApplication.java
@@ -2,14 +2,11 @@ package com.example.moneymanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(
-		exclude = {
-				org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration.class
-		}
-)
+@SpringBootApplication(exclude = {MailSenderAutoConfiguration.class})
 public class MoneymanagerApplication {
 
 	public static void main(String[] args) {
